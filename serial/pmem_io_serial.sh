b5b482b52484842e1be7ebeea8d65b8d25c0d78b
ccsoni <kohji.mailbox@gmail.com>
2024-03-05 16:13:25 +0900

    Shell script modified.

--- serial/pmem_io_serial.sh
+++ serial/pmem_io_serial.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 nshift=(10 12 14 16 18 20 22 24 26 28)
-nomp=(16, 32)
+nomp=(16 32)
 
 for nomp_ in "${nomp[@]}" ; do
 	for nshift_ in "${nshift[@]}" ; do
 		echo ${nshift_} ${nomp_}
-		./pmem_io_single ${nshift_} ${nomp_} > pmem_io_single_${nshift_}_${nomp_}.dat
+		./pmem_io_serial ${nshift_} ${nomp_} > pmem_io_serial_${nshift_}_${nomp_}.dat
 	done
 done
